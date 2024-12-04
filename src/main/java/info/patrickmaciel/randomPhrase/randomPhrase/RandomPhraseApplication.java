2b7ba05c2415e065015911d4c6fe248d924e1d77
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-12-04 16:24:27 -0300

    feat: initial commit

---
+++ src/main/java/info/patrickmaciel/randomPhrase/randomPhrase/RandomPhraseApplication.java
@@ -0,0 +1,13 @@
+package info.patrickmaciel.randomPhrase.randomPhrase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPhraseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPhraseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/patrickmaciel/randomPhrase/randomPhrase/RandomPhraseApplicationTests.java
@@ -0,0 +1,13 @@
+package info.patrickmaciel.randomPhrase.randomPhrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPhraseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
